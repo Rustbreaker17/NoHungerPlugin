4f845383b106494725e924639bf220c7eaa1f6f3
Rustbreaker17 <128778500+Rustbreaker17@users.noreply.github.com>
2023-06-07 20:22:20 +0530

    Add files via upload

---
+++ NoHungerPlugin/src/main/java/rustbreaker/nohungerplugin/NoHungerPlugin.java
@@ -0,0 +1,29 @@
+package rustbreaker.nohungerplugin;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoHungerPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin cleanup
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            player.setFoodLevel(20); // Set player's food level to maximum (20) to remove hunger
+            event.setCancelled(true); // Cancel the event to prevent the food level from changing
+        }
+    }
+}
